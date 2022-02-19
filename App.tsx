8b067051fdbf0af2bc7e580e277411b28a51afd7
Lukas Haring <lukasharing123@gmail.com>
2022-02-20 01:56:03 +0200

    adding radio buttons, types are not perfect yet

--- App.tsx
+++ App.tsx
@@ -1,15 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
 import { styles } from './Styles/global-style'
-import { Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { Text, View, SafeAreaView, ScrollView, Button } from 'react-native';
 import { Header } from './Components/Header';
 import { Footer } from './Components/Footer';
+import react, { useState } from 'react';
+import { RadioButton } from './Components/RadioButton';
+
+const options: {key: string, text: string}[] = [
+  {
+    key: 'male',
+    text: 'Male',
+  },
+  {
+    key: 'female',
+    text: 'Female',
+  },
+];
+
+const state = { value: null }
 
 export default function App() {
   
+  const [selectedOption, setSelectedOption] = useState<({key: string, text: string}) | null>()
+  
+
+  const onSelect = (item: {key: string, text: string}) => {
+    if (selectedOption && selectedOption.key === item.key) {
+      setSelectedOption(null);
+    } else {
+      setSelectedOption(item);
+    }
+  };
+  const onSubmit = () => {
+    console.log(selectedOption);
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Header />
+        <RadioButton
+          selectedOption={selectedOption}
+          onSelect={onSelect}
+          options={options}
+        />
+        <Button title="SUBMIT" onPress={onSubmit} />
         <Footer />
       </ScrollView>
       <StatusBar style="light" />
--- Styles/global-style.tsx
+++ Styles/global-style.tsx
@@ -15,10 +15,32 @@ export const styles = StyleSheet.create({
   },
   headerText: {
     fontSize: 40,
+    fontWeight: 'bold',
     color: '#30a2c5',
   },
   text: {
     fontSize: 15,
     color: '#fff',
-  }
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  circle: {
+      height: 20,
+      width: 20,
+      borderRadius: 10,
+      borderWidth: 1,
+      borderColor: '#ACACAC',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+  checkedCircle: {
+      width: 14,
+      height: 14,
+      borderRadius: 7,
+      backgroundColor: '#794F9B',
+  },
 });
